7f48cf8a94631bee6c42b246ef306c84b7638fb6
Jordan <jordanchong23@gmail.com>
2016-05-06 13:24:23 -0700

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,35 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(obj === null){
+  	return 'null';
+  }else if(typeof obj === 'boolean'){
+  	return String(obj);
+  }else if(typeof obj === 'number'){
+  	return String(obj);
+  }else if(typeof obj === 'string'){
+  	return '"' + obj + '"';
+  }else if(typeof obj === 'object'){
+  	if(Array.isArray(obj)){
+      var arrString = _.map(obj, function(item){
+  	  	if(typeof obj === 'undefined' || typeof obj === 'function'){
+  	  	  item = null;
+  	  	}
+  	  	return stringifyJSON(item);
+  	  }).join(',');
+  	  return '[' + arrString + ']';
+  	}else{
+  	  for(var i in obj){
+  	  	if(typeof obj[i] === 'function'){
+  	  	  return '{}';
+  	  	}
+  	  }
+  	  var objString = _.map(obj, function(item, key){
+  	  	return stringifyJSON(key) + ':' + stringifyJSON(item);
+  	  }).join(',');
+  	  return '{' + objString + '}';
+  	}
+  }else if(typeof obj === 'undefined' || typeof obj === 'function'){
+  	return undefined;
+  }
 };
