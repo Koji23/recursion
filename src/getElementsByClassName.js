378aff961b9dc3b39bef2f0248cf9054db16386d
Jordan <jordanchong23@gmail.com>
2016-05-06 11:26:45 -0700

    getElementsByClass appears to work but doesn't pass prompt(?)

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,34 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var results = [];
+
+  function recursiveHelper(node){
+  	if(node.className == className){
+  	  results.push(node);
+  	}
+  	for(var i=0; i<node.childNodes.length; i++){
+  	    recursiveHelper(node.childNodes[i]);	
+  	  }
+  }
+  recursiveHelper(document.documentElement);
+  return results;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
