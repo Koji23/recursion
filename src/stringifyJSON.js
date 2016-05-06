2142b729e98a1458ce84d3d5056379cc1dc4c32a
Jordan <jordanchong23@gmail.com>
2016-05-06 16:23:39 -0700

    Comment and reorganize 'if' blocks for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,34 +6,41 @@
 var stringifyJSON = function(obj) {
   // your code goes here
   if(obj === null){
-  	return 'null';
+  	return String(null);
   }else if(typeof obj === 'boolean'){
   	return String(obj);
   }else if(typeof obj === 'number'){
   	return String(obj);
   }else if(typeof obj === 'string'){
-  	return '"' + obj + '"';
+  	//strings should get redundent quotes when stringified (necessary for object keys)
+    return '"' + obj + '"';
+  }else if(typeof obj === 'undefined' || typeof obj === 'function'){
+    //'functions' and 'undefined' objects are not stringifiable, they should return undefined
+    return undefined;
   }else if(typeof obj === 'object'){
   	if(Array.isArray(obj)){
+      //arrays
       var arrString = _.map(obj, function(item){
   	  	if(typeof obj === 'undefined' || typeof obj === 'function'){
-  	  	  item = null;
+  	  	  //*special behavior: unstringifiable objects should return 'null' instead of undefined when inside Arrays
+          item = null;
   	  	}
-  	  	return stringifyJSON(item);
+  	  	return stringifyJSON(item); //recursive call the stringifyJSON 
   	  }).join(',');
   	  return '[' + arrString + ']';
   	}else{
+      //plain objects
   	  for(var i in obj){
   	  	if(typeof obj[i] === 'function'){
-  	  	  return '{}';
+  	  	  //*special behavior: if an object has a function inside of it, drop everything and return an empty object
+          //note: 'undefined' does not trigger a similar behavior when inside an object
+          return '{}';
   	  	}
   	  }
   	  var objString = _.map(obj, function(item, key){
-  	  	return stringifyJSON(key) + ':' + stringifyJSON(item);
+  	  	return stringifyJSON(key) + ':' + stringifyJSON(item); //recursive call the stringifyJSON 
   	  }).join(',');
   	  return '{' + objString + '}';
   	}
-  }else if(typeof obj === 'undefined' || typeof obj === 'function'){
-  	return undefined;
   }
 };
